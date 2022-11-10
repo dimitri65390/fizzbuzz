73312a58a40a0606227f5388bdc1cc72255657b6
dimitri65390 <116097430+dimitri65390@users.noreply.github.com>
2022-11-10 10:09:41 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+let b = 0
+let whalla = 0
+let num = 0
+let binks = 0
+let a = 0
+basic.forever(function () {
+    while (num == 1000) {
+        if (num >= 100) {
+            num += 1
+        } else {
+            num = 0
+        }
+        basic.showNumber(num)
+        whalla = num
+        binks = num
+        while (binks <= 0) {
+            if (0 == 0) {
+            	
+            } else {
+            	
+            }
+        }
+        while (whalla <= 0) {
+            if (whalla == 0) {
+                a = 1
+            } else {
+                whalla += -3
+            }
+        }
+    }
+})
